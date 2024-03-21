6b36a3a2b39919b7808292ab28013be10fcffaa2
CrioUser <crio-user@criodo.com>
2024-03-21 10:59:59 +0530

    First Commit

--- pl_java/exercise_1/CustomTime.java
+++ pl_java/exercise_1/CustomTime.java
@@ -1,10 +1,88 @@
 package pl_java.exercise_1;
+import java.time.LocalTime;
 
 public class CustomTime{
 
-    int hour;   // current hour in military time
-    int minute; // current minute in military time
-    int second; // current second in military time
+    private int hour;
+    private int minute;
+    private int second;
+
+   
+/*
+* Constants
+*/
+final static int MAX_HOURS = 23;
+final static int MAX_MINS = 59;
+final static int MAX_SECS = 59;
+
+/*
+* Setters
+*/
+private void setHour(int hour) {
+    if (hour >= 0 && hour <= MAX_HOURS) {
+        this.hour = hour;
+    }
+    else {
+        System.out.println("Error: hour must be between 0 and 23 inclusive!");
+        hour = 0;
+    }
+}
+
+private void setMinute(int minute) {
+    if (minute >= 0 && minute <= MAX_MINS) {
+        this.minute = minute;
+    }
+    else {
+        System.out.println("Error: minute must be between 0 and 59 inclusive!");
+        minute = 0;
+    }
+}
+
+private void setSecond(int second) {
+    if (second >= 0 && second <= MAX_SECS) {
+        this.second = second;
+    }
+    else {
+        System.out.println("Error: second must be between 0 and 59 inclusive!");
+        second = 0;
+    }
+}
+
+
+/*
+* Getters
+*/
+public int getHour() {
+    return hour;
+}
+
+public int getMinute() {
+    return minute;
+}
+
+public int getSecond() {
+    return second;
+}
+
+public CustomTime(int hour, int minute, int second){
+    setHour(hour);
+    setMinute(minute);
+    setSecond(second);
+}
+
+
+/*
+* Constructors
+*/
+   // Default constructor which returns current time on initialization
+   public CustomTime(){
+    LocalTime time = LocalTime.now();
+    hour = time.getHour();
+    minute = time.getMinute();
+    second = time.getSecond();
+}
+
+
   
     // return the time to the calling method in a three-membered array
     int[] getTime(){
@@ -12,7 +90,18 @@ public class CustomTime{
     }
 
     // increment the current time by one second  
-    void incrementTime(){
-        ++second;
+    
+/*
+* Methods
+*/
+   // increment the current time by one second
+   void incrementTime(){
+    second = ++second % (MAX_SECS + 1);
+    if(second == 0) {
+        minute = ++minute % (MAX_MINS + 1);
     }
+    if(second == 0 && minute == 0) {
+        hour = ++hour % (MAX_HOURS + 1);
+    }
+}
 }
--- pl_java/exercise_2/Profile.java
+++ pl_java/exercise_2/Profile.java
@@ -1,53 +1,109 @@
 package pl_java.exercise_2;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+
+
+
 
 public class Profile {
 
 /*
  * Attributes
  */
-    String name;
-    String countryCode;
-    String phoneNo;
-    String profilePhotoUrl;
-    String about;
-    String lastSeen;
+    private String name;
+    private String countryCode;
+    private String phoneNo;
+    private URI profilePhotoUrl;
+    private String about;
+    private LocalDateTime lastSeen;
 
 
 /*
  * Constructors
  */
+public Profile(String name, String countryCode, String phoneNo, String profilePhotoUrl)throws URISyntaxException
+{
+        setName(name);
+        setPhoneNo(countryCode, phoneNo);
+        setProfilePhotoUrl(profilePhotoUrl);
+        this.lastSeen = LocalDateTime.now();
+}
+
+public Profile(String name, String countryCode, String phoneNo) throws URISyntaxException
+{
+        setName(name);
+        setPhoneNo(countryCode, phoneNo);
+        setProfilePhotoUrl("https://pps.whatsapp.net/v/noDP.jpg");
+        this.lastSeen =LocalDateTime.now();
+        setAbout("Hey there I am using whatsapp");
+}
 
 
 /*
  * Getters
  */
+public String getName(){
+        return name;
+    }
+    public String getPhone(){
+        return countryCode + " "+ phoneNo;
+    }
+    public URI getProfilePhotoUrl(){
+        return profilePhotoUrl;
+    }
+    public String getAbout(){
+        return about;
+    }
+    public LocalDateTime getLastseen(){
+        return lastSeen;
+    }
 
 
 /*
  * Setters
  */
     public void setName(String name){
-            this.name = name;
+        if(name.isBlank()){
+                System.out.println("Error: name cnt't be blank");
+        }else{ 
+                this.name = name;
+        }
     }
 
     public void setPhoneNo(String countryCode, String phoneNo){
+        if(phoneNo.length() < 10){
+                System.out.println("Error: phone number  must be 10 digits");
+        }else{
             this.countryCode = countryCode;
             this.phoneNo = phoneNo;
+        }
     }
 
-    public void setProfilePhotoUrl(String profilePhotoUrl){
-        this.profilePhotoUrl = profilePhotoUrl;
+    public void setProfilePhotoUrl(String profilePhotoUrl) throws URISyntaxException{
+        this.profilePhotoUrl = new URI(profilePhotoUrl);
     }
 
     public void setAbout(String about){
+        if(about.isBlank()){
+                System.out.println("Error : About can not be blank");
+        }else{
             this.about = about;
+        }
     }
+
+
 
 
 /*
  * Methods
  */
-    public void updateLastSeen(String lastSeen){
-            this.lastSeen = lastSeen;
+    public void updateLastSeen(LocalDateTime lastSeen){
+        if(this.lastSeen.isAfter(lastSeen)){
+                System.out.println("Error: current lastSeen cannot be more than input lastSeen!");
+            }else{
+                this.lastSeen = lastSeen;
+            }
+    
     }
 }
